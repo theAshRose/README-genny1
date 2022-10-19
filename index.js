f908b3f03906ca66ff20766cff43c71ae4136abe
theDomconrad <them.and.us.2013@gmail.com>
2022-10-18 21:30:44 -0700

    made progress of questions

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer")
+const getFunctions = require("./utils/generateMarkdown.js")
+
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Would you like to create a proffesional README.md",
+    "Fantastic, first lets pick a name for your actual .md file. Note: we will add the '.md' for you.",
+    "What is the title of your project/repo?",
+    "Please write a short paragraph describing your app:",
+    "Please insert a link or file pathway for a screenshot: ",
+    "Write a description for your screenshot",
+    ""
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
@@ -12,13 +25,50 @@ function init() { }
 // Function call to initialize app
 init();
 inquirer.prompt([{
-    name: "test1",
-    message: "message1",
-    type: "input1",
+    name: "ifUserReadme",
+    message: "Would you like to create a proffesional README.md",
+    type: "confirm",
 }, {
-    name: "test2",
-    message: "message1",
-    type: "test2",
+    name: "readmeFileName",
+    message: "Fantastic, first lets pick a name for your actual .md file. Note: we will add the '.md' for you.",
+    type: "input",
+}, {
+    name: "title",
+    message: "What is the title of your project/repo?",
+    type: "input",
+}, {
+    name: "about",
+    message: "Please write a short paragraph describing your app: ",
+    type: "input",
+}, {
+    name: "SS",
+    message: "Please insert a link or file pathway for a screenshot: ",
+    type: "input",
+}, {
+    name: "ssDesc",
+    message: "Write a description for your screenshot",
+    type: "input",
+}, {
+    name: "screenShot",
+    message: "Fantastic, please intsert your pathway or link.",
+    type: "input",
+}, {
+    name: "screenShot",
+    message: "Fantastic, please intsert your pathway or link.",
+    type: "input",
+}, {
+    name: "screenShot",
+    message: "Fantastic, please intsert your pathway or link.",
+    type: "input",
 }]).then(function (data) {
-    console.log(data)
+    if (data.ifUserReadme === true) {
+        const fs = require("fs")
+        const pathfinder = require("path")
+        fs.writeFile(pathfinder.join("C:\\Users\\dom18\\desktop\\bootcamp\\README-genny1\\" + data.readmeFileName +".md"), "Testresult", (event) => { console.log("ERROR") })
+        console.log(getFunctions(data))
+    }
 })
+
+///////title
+/////////about
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,20 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -8,13 +25,27 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  console.log("poopypoopy")
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  return `# ${data.title}
+  
+  
+ ## ${data.about}
+
+ ## Table of Contents:
+
+ ## ${data.title}
+ ![screenshot](${data.screenShot})
+
+
+
+
+  `; 
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown, renderLicenseSection};
